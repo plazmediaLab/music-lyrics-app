27d0606d14d4e0b3281e65ebb7efb6eabf6cbd74
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-02-20 20:16:46 -0600

    Mansory Layout provado con petición fetch | Funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
 
 function App() {
+
+  // STATE
+  const [resultados, guardarResultados] = useState([]);
+  const [consulta, guardarConsultaRealizada] = useState(false);
+  
+  useEffect(() => {
+    
+    if(consulta) return;
+    
+    const consultarAPI = async () => {
+      let search = 'canon';
+      let page = 1;
+      let perPage = 100;
+      // let key = 'f9XhO7jggni_s-mvQhsbghVgVleYbqALvb0p4YCEwPw';
+      // let url = `https://api.unsplash.com/search/photos?client_id=${key}&per_page=${perPage}&page=${page}&query=${search}`;
+
+      const imagenesPorPagina = 30;
+      const key = '15267306-f7d4977593e90dfd66b28ce54';
+      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${perPage}&page=${page}`;
+      
+      const consulta = await fetch(url);
+      const resultados = await consulta.json();
+      
+      guardarResultados(resultados.hits)
+    };
+    guardarConsultaRealizada(true)
+  
+    consultarAPI()
+  }, [consulta]);  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Music Lyrics APP</h1>
+      <div className="container mansory-layout" id="mansory-layout" columns="4">
+         {/* {resultados.map(item => (
+            <div className="mansory-layout__item" key={item.id}>
+              <img src={item.urls.small} alt={item.color} />
+            </div>
+          ))} */}
+         {resultados.map(item => (
+            <div className="mansory-layout__item" key={item.id}>
+              <img src={item.previewURL} alt={item.tags} />
+            </div>
+          ))}
+      </div>
+    </Fragment>
   );
 }
 
